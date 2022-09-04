02f4cbc2f727831e0b73cd56d369a2b886c646a9
alberto.niironen <alberto.niironen@gmail.com>
2022-09-05 00:29:40 +0100

    [ADD] index minor changes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
+mongoose
+  .connect(config.mongoose.url, config.mongoose.options)
+  .then(() => {
+    logger.info('Connected to MongoDB');
+    server = app.listen(config.port, () => {
+      logger.info(`Listening to port ${config.port}`);
+    });
+  })
+  .catch(() => {
+    logger.error('Database connection error');
   });
-});
 
 const exitHandler = () => {
   if (server) {
